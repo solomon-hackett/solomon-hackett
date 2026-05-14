305b628403e29bd3206b4549819fd54f9a8464c3
solomon-hackett <solomonhackett2008@gmail.com>
2026-05-14 10:37:07 +0100

    added my tryhackme profile iframe and fixed stickiness of the nav

--- app/ui/home/about.tsx
+++ app/ui/home/about.tsx
@@ -1,9 +1,13 @@
-import { spaceGrotesk } from '@/app/lib/fonts';
+import { spaceGrotesk } from "@/app/lib/fonts";
 
 export default function About() {
   return (
     <div>
       <h1 className={`${spaceGrotesk.className}`}>About Me</h1>
+      <iframe
+        src="https://tryhackme.com/api/v2/badges/public-profile?userPublicId=1916860"
+        style={{ border: "none", width: "100%" }}
+      ></iframe>
     </div>
   );
 }
